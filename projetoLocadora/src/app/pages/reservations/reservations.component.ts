4003ae46983f625b7d526a11d1b41e6c76f4f0e8
FelipeRonzani <113400758+FelipeRonzani@users.noreply.github.com>
2023-11-19 20:36:16 -0300

    Update reservations.component.ts

--- projetoLocadora/src/app/pages/reservations/reservations.component.ts
+++ projetoLocadora/src/app/pages/reservations/reservations.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationsService } from './reservations.service';
 
 @Component({
+  selector: 'app-reservations',
   templateUrl: './reservations.component.html',
-  styleUrls: ['./reservations.component.scss']
-})
-export class ReservationsComponent {
+  styleUrls: ['./reservations.component.css'],
+  })
+export class ReservationsComponent implements OnInit{
+  reservations: { vehicles: any; clients: any }[] = [];
 
+  constructor(private reservationsService: ReservationsService){}
+
+  ngOnInit(){
+  this.reservations = this.reservationsService.getReservations();
+}
 }
