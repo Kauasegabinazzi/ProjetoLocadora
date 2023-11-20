cec2c9c89232260b8078dadef238321959b9ea53
FelipeRonzani <113400758+FelipeRonzani@users.noreply.github.com>
2023-11-20 10:31:55 -0300

    Add files via upload

---
+++ projetoLocadora/src/app/pages/reservations/error-dialog.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from './error-dialog.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorDialogService {
+  constructor(private dialog: MatDialog) {}
+
+  openDialog(errorMessage: string): void {
+    this.dialog.open(ErrorDialogComponent, {
+      width: '250px',
+      data: errorMessage,
+    });
+  }
+}
