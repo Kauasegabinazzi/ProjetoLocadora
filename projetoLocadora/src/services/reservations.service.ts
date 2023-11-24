2927bb52c06ef46d696018115c6cfbf913ed7476
FelipeRonzani <113400758+FelipeRonzani@users.noreply.github.com>
2023-11-24 13:34:55 -0300

    atualizando reservations.service.ts

--- projetoLocadora/src/services/reservations.service.ts
+++ projetoLocadora/src/services/reservations.service.ts
@@ -1,96 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ErrorDialogService } from '../../../services/errordialog.service';
-import { ReservationList } from 'src/app/pages/reservations/module/struct';
-import { ClientService } from 'src/services/client.service';
-import { VehicleService } from 'src/services/vehicle.service';
-import { ReservationsService } from '../../../services/reservations.service';
-import { Subscription, Observable, BehaviorSubject } from 'rxjs';
-import { VehicleList } from '../vehicles/module/struct';
-import { ClientList } from '../clients/module/struct';
+import { VehicleList } from 'src/app/pages/vehicles/module/struct';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-type ValidProperties = 'vehicles' | 'clients' | 'reservations';
-type Reservation = ReservationList;
+export class VehicleService {
 
-@Component({
-  selector: 'app-reservations',
-  templateUrl: './reservations.component.html',
-  styleUrls: ['./reservations.component.scss']
-})
-export class ReservationsComponent implements OnInit, OnDestroy {
-  vehicles: VehicleList[] = [];
-  clients: ClientList[] = [];
-  selectedClient: number = 0;
-  selectedVehicle: number = 0;
-  reservations: ReservationList[] = [];
+  private url: string = 'http://localhost:3000';
 
-  private subscriptions: Subscription[] = [];
-  private reservationsSubject: BehaviorSubject<Reservation[]> = new BehaviorSubject<Reservation[]>([]);
-  reservations$ = this.reservationsSubject.asObservable();
+  constructor(private http: HttpClient) {}
 
-
-  constructor(private VehicleService: VehicleService, private ReservationsService: ReservationsService, private errorDialogService: ErrorDialogService, private ClientService: ClientService) { }
-
-  ngOnInit(): void {
-    this.loadInitialData();
-    this.ReservationsService.reservationList$.subscribe(reservations => {
-      this.reservations = reservations;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
-  }
-
-  loadInitialData(): void {
-    this.subscriptions.push(
-      this.loadData('vehicles', this.vehicles, this.VehicleService.VehicleList.bind(this.ReservationsService)),
-      this.loadData('clients', this.clients, this.ClientService.ClientList.bind(this.ReservationsService)),
-      this.loadData('reservations', this.reservations, this.ReservationsService.ReservationList.bind(this.ReservationsService))
-    );
-  }
-
-  loadData(property: ValidProperties, data: any[], method: () => Observable<any[]>): Subscription {
-    return method().subscribe((response: any) => {
-      if (response) {
-        this[property] = response;
-      }
-    }, (error: any) => {
-      this.errorDialogService.openDialog(`Erro ao carregar ${property}: ${error.message}`);
-    });
-  }
-  
-  reserveVehicle(): void {
-    if (this.selectedClient !== 0 && this.selectedVehicle !== 0) {
-      const existingReservation = this.reservationsSubject.getValue().find(
-        (reservation: Reservation) => reservation.vehicleid === this.selectedVehicle && reservation.id === this.selectedClient
-      );
-
-      if (!existingReservation) {
-        this.ReservationsService.ReservationAdd(this.selectedClient, this.selectedVehicle)
-          .subscribe((response: any) => {
-            if (response) {
-              const newReservation: Reservation = {
-                id: this.selectedClient,
-                vehicleid: this.selectedVehicle
-              };
-              this.reservationsSubject.next([...this.reservationsSubject.getValue(), newReservation]);
-              this.selectedClient = 0;
-              this.selectedVehicle = 0;
-            } else {
-              this.errorDialogService.openDialog('Falha ao reservar o veículo.');
-            }
-          });
-      }
-    }
-  }
-
-  getClientName(clientid: number): string {
-    const client = this.clients.find(c => c.id === clientid);
-    return client ? client.name : 'Cliente Desconhecido';
-  }
-
-  getVehicleModel(vehicleid: number): string {
-    const vehicle = this.vehicles.find(v => v.id === vehicleid);
-    return vehicle ? vehicle.model : 'Veículo Desconhecido';
+  public VehicleList(): Observable<Array<VehicleList>> {
+    return this.http.get<Array<VehicleList>>(`${this.url}/list-vehicles`).pipe(
+      res => res,
+      error => error
+    )
   }
 }
