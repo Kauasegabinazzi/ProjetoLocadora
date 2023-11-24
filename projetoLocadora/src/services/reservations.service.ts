e7ef60be96584a05b2e478528f9905e0c870eb37
FelipeRonzani <113400758+FelipeRonzani@users.noreply.github.com>
2023-11-24 12:54:42 -0300

    Reorganizando

--- projetoLocadora/src/services/reservations.service.ts
+++ projetoLocadora/src/services/reservations.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { ClientList, VehicleList, ReservationList } from '../module/struct';
+import { ReservationList } from 'src/app/pages/reservations/module/struct';
 
 @Injectable({
   providedIn: 'root'
@@ -22,18 +22,13 @@ export class FakeApiService {
 
   constructor(private http: HttpClient) {}
 
-  getClientList(): Observable<Array<ClientList>> {
-    return this.http.get<Array<ClientList>>(`${this.baseUrl}/list-clients`);
-  }
-
-  getVehicleList(): Observable<Array<VehicleList>> {
-    return this.http.get<Array<VehicleList>>(`${this.baseUrl}/list-vehicles`);
+  getReservationList(): Observable<Array<ReservationList>> {
+    return this.http.get<Array<ReservationList>>(`${this.baseUrl}/list-reservations`).pipe(
+      res => res,
+      error => error
+    )
   }
   
-  getReservationList(): Observable<Array<ReservationList>> {
-    return this.http.get<Array<ReservationList>>(`${this.baseUrl}/list-reservations`);
-  }
-
   addReservation(clientid: number, vehicleid: number): Observable<ReservationList> {
     const newReservation = {
       id: clientid,
