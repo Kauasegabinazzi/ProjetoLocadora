82e2a3a389975a80d8c2e6dfa6e9e3ca013bdc61
FelipeRonzani <113400758+FelipeRonzani@users.noreply.github.com>
2023-11-24 14:10:00 -0300

    Update reservations.service.ts

--- projetoLocadora/src/services/reservations.service.ts
+++ projetoLocadora/src/services/reservations.service.ts
@@ -1,17 +1,46 @@
-import { VehicleList } from 'src/app/pages/vehicles/module/struct';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { ReservationList } from 'src/app/pages/reservations/module/struct';
 
-export class VehicleService {
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
 
-  private url: string = 'http://localhost:3000';
+  private baseUrl: string = 'http://localhost:3000';
+  private reservationsUrl: string = `${this.baseUrl}/list-reservations`;
+  private reservationListSubject: BehaviorSubject<ReservationList[]> = new BehaviorSubject<ReservationList[]>([]);
+  public reservationList$: Observable<ReservationList[]> = this.reservationListSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
-  public VehicleList(): Observable<Array<VehicleList>> {
-    return this.http.get<Array<VehicleList>>(`${this.url}/list-vehicles`).pipe(
+  public ReservationList(): Observable<Array<ReservationList>> {
+    return this.http.get<Array<ReservationList>>(`${this.baseUrl}/list-reservations`).pipe(
       res => res,
       error => error
     )
+  }
+
+  public ReservationListUpdate(): void {
+    this.ReservationList().subscribe((reservations: ReservationList[]) => {
+      this.reservationListSubject.next(reservations);
+    });
+  }
+
+  public ReservationAdd(clientid: number, vehicleid: number): Observable<ReservationList> {
+    const newReservation = {
+      id: clientid,
+      vehicleid: vehicleid,
+    };
+
+    return this.http.post<any>(this.reservationsUrl, newReservation).pipe(
+      tap((response: any) => {
+        if (response && response.success) {
+          this.ReservationListUpdate();
+        }
+      })
+    );
   }
 }
